debbf6e9d4f1b8ecbe06d7a564e31ae365fface0
Mahim Ali <117038967+Mahimali937@users.noreply.github.com>
2024-07-25 11:13:36 -0400

    Initial commit for empty repository

---
+++ src/main/java/com/example/UserAuth/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.UserAuth.service;
+
+import com.example.UserAuth.model.User;
+import com.example.UserAuth.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository, EmailService emailService) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}
